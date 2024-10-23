3b1617d4065a4ceff2541b0807daff57d5ea908c
srt-2000 <srt2000888@gmail.ru>
2024-10-23 07:00:37 +0300

    update gitignore

--- testing_file.py
+++ testing_file.py
@@ -1,12 +1,1 @@
-n = int(input())
-temp = []
-temp.append(int(n))
-response = 0
-for line in range(n):
-    temp.append(int(input()))
-qnt_ab_one = sum(temp[1::])
-qnt_ab_double = 0
-for i in temp[1::]:
-    if i // 2 != 0:
-        qnt_ab_double += i // 2
-print(qnt_ab_double // qnt_ab_one)
+#for testing how code works
